783a4dda895d7333958976cf0d4bef5a374c1e51
thiago <thiagohissa@gmail.com>
2020-10-08 19:30:45 -0400

    question 2 done

---
+++ question-2/callbacks.js
@@ -0,0 +1,29 @@
+function resolvedPromise(){
+    return new Promise(((resolve) => {
+        setTimeout(function(){
+            let success = {message: 'delayed success'};
+            resolve(success);
+        }, 500);
+    }));
+}
+
+function rejectedPromise(){
+    return new Promise(((resolve, reject) => {
+        setTimeout(function(){
+            try{
+                throw new Error('error: delayed exception');
+            }
+            catch (err) {
+                return reject(err);
+            }
+        }, 500);
+    }));
+}
+
+resolvedPromise().then((resolve =>{
+    console.log(resolve);
+})).catch(()=> { console.log("Something went wrong"); }); // catching for the sake of it
+
+rejectedPromise().catch((err) => {
+    console.log(err.message);
+});
